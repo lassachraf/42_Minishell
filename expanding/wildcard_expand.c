e5efa6530cbf7558749e51bac2ae75c6620a2210
alassiqu <alassiqu@c4r6p2.1337.ma>
2024-06-22 21:50:00 +0100

    Small update !

--- expanding/wildcard_expand.c
+++ expanding/wildcard_expand.c
@@ -6,93 +6,224 @@
 /*   By: alassiqu <alassiqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:54:58 by alassiqu          #+#    #+#             */
-/*   Updated: 2024/06/21 21:38:06 by alassiqu         ###   ########.fr       */
+/*   Updated: 2024/06/22 21:49:40 by alassiqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
+// char	*ft_strcpy(char *dest, char *src)
+// {
+// 	int	i;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+// 	i = 0;
+// 	while (src[i] != '\0')
+// 	{
+// 		dest[i] = src[i];
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
+
+// void	append_to_result(char **result, char *str)
+// {
+// 	size_t	len;
+// 	size_t	str_len;
+// 	char	*new_result;
+
+// 	len = 0;
+// 	if (*result)
+// 		len = ft_strlen(*result);
+// 	str_len = ft_strlen(str);
+// 	new_result = malloc(len + str_len + 2);
+// 	if (!new_result)
+// 		return ;
+// 	if (*result)
+// 	{
+// 		ft_strcpy(new_result, *result);
+// 		free(*result);
+// 	}
+// 	ft_strcpy(new_result + len, str);
+// 	new_result[len + str_len] = '\n';
+// 	new_result[len + str_len + 1] = '\0';
+// 	*result = new_result;
+// }
+
+// int	match_pattern(const char *pattern, const char *filename)
+// {
+// 	while (*pattern && *filename)
+// 	{
+// 		if (*pattern == '*')
+// 		{
+// 			if (*(pattern + 1) == '\0')
+// 				return (1);
+// 			while (*filename)
+// 			{
+// 				if (match_pattern(pattern + 1, filename++))
+// 					return (1);
+// 			}
+// 			return (0);
+// 		}
+// 		else if (*pattern != *filename)
+// 		{
+// 			return (0);
+// 		}
+// 		pattern++;
+// 		filename++;
+// 	}
+// 	return ((*pattern == '*' && *(pattern + 1) == '\0')
+// 		|| (*pattern == *filename));
+// }
+
+// void	asterisk_expand(t_token *token)
+// {
+// 	DIR				*dir;
+// 	struct dirent	*entry;
+// 	char			*dir_path;
+// 	char			*result;
+
+// 	dir_path = ".";
+// 	result = NULL;
+// 	dir = opendir(dir_path);
+// 	if (!dir)
+// 		return ;
+// 	entry = readdir(dir);
+// 	while (entry)
+// 	{
+// 		if (match_pattern(token->value, entry->d_name))
+// 			append_to_result(&result, entry->d_name);
+// 		entry = readdir(dir);
+// 	}
+// 	closedir(dir);
+// 	if (result)
+// 	{
+// 		free(token->value);
+// 		token->value = result;
+// 		token->type = WORD;
+// 	}
+// }
+
+////////////////////////////////////////////////////////////////////////////
+
+char *ft_strcpy(char *dest, const char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
 }
 
-void	append_to_result(char **result, char *str)
-{
-	size_t	len;
-	size_t	str_len;
-	char	*new_result;
+size_t ft_strlen(const char *str) {
+    size_t len = 0;
+    while (str[len])
+        len++;
+    return len;
+}
+
+void append_to_result(char **result, const char *str) {
+    size_t len;
+    size_t str_len;
+    char *new_result;
 
 	len = 0;
-	if (*result)
-		len = ft_strlen(*result);
 	str_len = ft_strlen(str);
-	new_result = malloc(len + str_len + 2);
-	if (!new_result)
-		return ;
-	if (*result)
+    if (*result)
+        len = ft_strlen(*result);
+    new_result = malloc(len + str_len + 2);
+    if (!new_result)
+        return;
+    if (*result) 
 	{
-		ft_strcpy(new_result, *result);
-		free(*result);
-	}
-	ft_strcpy(new_result + len, str);
-	new_result[len + str_len] = '\n';
-	new_result[len + str_len + 1] = '\0';
-	*result = new_result;
+        ft_strcpy(new_result, *result);
+        free(*result);
+    }
+    ft_strcpy(new_result + len, str);
+    new_result[len + str_len] = '\n';
+    new_result[len + str_len + 1] = '\0';
+    *result = new_result;
 }
 
-int	match_pattern(const char *pattern, const char *filename)
-{
-	while (*pattern && *filename)
-	{
-		if (*pattern == '*')
-		{
-			if (*(pattern + 1) == '\0')
-				return (1);
-			while (*filename)
-			{
-				if (match_pattern(pattern + 1, filename++))
-					return (1);
-			}
-			return (0);
-		}
-		else if (*pattern != *filename)
-		{
-			return (0);
-		}
-		pattern++;
-		filename++;
-	}
-	return ((*pattern == '*' && *(pattern + 1) == '\0')
-		|| (*pattern == *filename));
+int match_pattern(const char *pattern, const char *filename) {
+    while (*pattern && *filename) {
+        if (*pattern == '*') {
+            if (*(pattern + 1) == '\0')
+                return 1;
+            while (*filename) {
+                if (match_pattern(pattern + 1, filename++))
+                    return 1;
+            }
+            return 0;
+        } else if (*pattern != *filename) {
+            return 0;
+        }
+        pattern++;
+        filename++;
+    }
+    return ((*pattern == '*' && *(pattern + 1) == '\0') || (*pattern == *filename));
 }
 
-void	asterisk_expand(t_token *token)
+void process_directory(char *dir_name, char **result) {
+    DIR *dir;
+    struct dirent *entry;
+    char *dir_header;
+    size_t len;
+
+    dir = opendir(dir_name);
+    len = ft_strlen(dir_name);
+    if (!dir)
+        return;
+    dir_header = malloc(len + 3);
+    ft_strcpy(dir_header, dir_name);
+    dir_header[len] = ':';
+    dir_header[len + 1] = '\n';
+    dir_header[len + 2] = '\0';
+    append_to_result(result, dir_header);
+    free(dir_header);
+
+    entry = readdir(dir);
+    while (entry) {
+        if (entry->d_name[0] != '.') {
+            // Add two spaces before the entry name for indentation
+            char *indented_entry = malloc(ft_strlen(entry->d_name) + 3);
+            sprintf(indented_entry, "  %s", entry->d_name);
+            append_to_result(result, indented_entry);
+            free(indented_entry);
+        }
+        entry = readdir(dir);
+    }
+    append_to_result(result, "");
+    closedir(dir);
+}
+
+void asterisk_expand(t_token *token)
 {
-	DIR				*dir;
-	struct dirent	*entry;
-	char			*dir_path;
-	char			*result;
+	DIR		*dir;
+	struct dirent *entry;
+	char *dir_path;
+	char *result;
 
 	dir_path = ".";
 	result = NULL;
 	dir = opendir(dir_path);
 	if (!dir)
-		return ;
+		return;
 	entry = readdir(dir);
 	while (entry)
 	{
-		if (match_pattern(token->value, entry->d_name))
-			append_to_result(&result, entry->d_name);
+		if (entry->d_name[0] != '.' && match_pattern(token->value, entry->d_name))
+		{
+			DIR *subdir = opendir(entry->d_name);
+			if (subdir)
+			{
+				closedir(subdir);
+				process_directory(entry->d_name, &result);
+			}
+			else
+				append_to_result(&result, entry->d_name);
+		}
 		entry = readdir(dir);
 	}
 	closedir(dir);
