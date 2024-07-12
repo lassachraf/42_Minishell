ac9c64da5d9e40163067fd70bbfb1b351a24fd32
alassiqu <alassiqu@c4r7p9.1337.ma>
2024-07-12 19:03:32 +0100

    ** Expanding is done (functionality), still have to deal with norms. **

--- expanding/wildcard_expand.c
+++ expanding/wildcard_expand.c
@@ -6,60 +6,11 @@
 /*   By: alassiqu <alassiqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:54:58 by alassiqu          #+#    #+#             */
-/*   Updated: 2024/07/12 18:03:28 by alassiqu         ###   ########.fr       */
+/*   Updated: 2024/07/12 18:58:15 by alassiqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-char	*ft_strcpy(char *dest, const char *src)
-{
-	int	i;
-
-	i = -1;
-	while (src[++i])
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
-
-void	append_to_result(char **result, char *str, int newline)
-{
-	size_t	len;
-	size_t	str_len;
-	char	*new_result;
-
-	len = 0;
-	str_len = ft_strlen(str);
-	if (*result)
-		len = ft_strlen(*result);
-	new_result = malloc(len + str_len + (newline ? 2 : 3));
-	if (!new_result)
-		return ;
-	if (*result)
-	{
-		ft_strcpy(new_result, *result);
-		free(*result);
-	}
-	ft_strcpy(new_result + len, str);
-	if (newline == 1)
-	{
-		new_result[len + str_len] = '\n';
-		new_result[len + str_len + 1] = '\0';
-	}
-	else if (newline == 2)
-	{
-		new_result[len + str_len] = ':';
-		new_result[len + str_len + 1] = '\0';
-	}
-	else
-	{
-		new_result[len + str_len] = ' ';
-		new_result[len + str_len + 1] = ' ';
-		new_result[len + str_len + 2] = '\0';
-	}
-	*result = new_result;
-}
 
 int	match_pattern(const char *pattern, const char *filename)
 {
@@ -87,47 +38,25 @@ int	match_pattern(const char *pattern, const char *filename)
 		|| (*pattern == *filename));
 }
 
-void	sort_strings(char **strings, size_t count)
+void	asterisk_expand(t_token **tokens, t_token *curr)
 {
-	char	*temp;
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (i < count - 1)
-	{
-		j = i + 1;
-		while (j < count)
-		{
-			if (ft_strcmp(strings[i], strings[j]) > 0)
-			{
-				temp = strings[i];
-				strings[i] = strings[j];
-				strings[j] = temp;
-			}
-			j++;
-		}
-		i++;
-	}
-}
-
-void asterisk_expand(t_token **tokens, t_token *curr)
-{
-    DIR				*dir;
-    struct dirent	*entry;
+	DIR				*dir;
+	struct dirent	*entry;
 
 	dir = opendir(".");
-    if (!dir)
+	if (!dir)
 	{
-        return ;
+		return ;
 	}
 	entry = readdir(dir);
-    while (entry)
+	while (entry)
 	{
-        if (entry->d_name[0] != '.' && match_pattern(curr->value, entry->d_name))
-            add_token_middle(tokens, new_token(ft_strdup(entry->d_name), WORD), curr->prev);
+		if (entry->d_name[0] != '.'
+			&& match_pattern(curr->value, entry->d_name))
+			add_token_middle(tokens,
+				new_token(ft_strdup(entry->d_name), WORD), curr->prev);
 		entry = readdir(dir);
-    }
-    closedir(dir);
+	}
+	closedir(dir);
 	remove_token(tokens, curr);
 }
